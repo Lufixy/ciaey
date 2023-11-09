d4b46a2f1551911bb28b5f89284671ca9b306500
Fluxy <57494449+Lufixy@users.noreply.github.com>
2023-11-09 22:13:22 +0300

    Update card.js

--- src/components/global/card.js
+++ src/components/global/card.js
@@ -7,107 +7,58 @@ import { useState } from "react";
 export function SiteCard() {
     const [modalOpen, setModalOpen] = useState(false);
 
+
     const SellixEmbed = () => {
-  return (
-    <div id="sellix-container">
-      <div id="sellix-buttons-pointers-fix">
-        <style>
-          {[data-sellix-product] * {
-            pointerEvents: 'none';
-          }
-          [data-sellix-group] * {
-            pointerEvents: 'none';
-          }}
-        </style>
-      </div>
-      <div id="sellix-modal-64851e9501bbf" style={{ display: 'flex', position: 'fixed', top: '0px', left: '0px', width: '100%', height: '100%', zIndex: '99999' }}>
-        <div id="backdrop" className="sellix-backdrop"></div>
-        <style>
-          {`.sellix-iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-          }
-
-          .sellix-iframe-content {
-            height: 100%;
-          }
-
-          .sellix-iframe-wrapper {
-            top: 20px;
-            margin: auto;
-            width: 100%;
-            height: 100%;
-            zIndex: 1;
-          }
-
-          .sellix-iframe-loader-container {
-            zIndex: 1;
-            position: absolute;
-            top: 30%;
-            left: 50%;
-            transform: translate(-50%);
-            display: flex;
-            flexDirection: column;
-            alignItems: center;
-          }
-
-          .sellix-backdrop {
-            background: #00000075;
-            backdropFilter: 'blur(3px)';
-            width: 100%;
-            height: 100%;
-            position: absolute;
-          }
-
-          .sellix-fallback-button {
-            fontFamily: 'Open Sans';
-            fontSize: '14px';
-            fontWeight: 600;
-            color: 'white';
-            textDecoration: 'none';
-          }
-
-          .sellix-fallback-button-container {
-            position: absolute;
-            zIndex: 2;
-            display: none;
-            top: 50%;
-            height: 50px;
-            lineHeight: '40px';
-            maxHeight: '50px';
-            boxSizing: 'border-box';
-            left: 50%;
-            transform: 'translate(-50%, -50%)';
-            background: '#613bea';
-            padding: '.375rem .75rem';
-            borderRadius: '3px';
-          }`}
-        </style>
-        <div className="sellix-iframe-loader-container">
-          <img src="https://cdn.sellix.io/static/embed/loader.png" alt="Loader" className="sellix-iframe-loader" style={{ width: '35px', display: 'none' }} />
-        </div>
-        <div className="sellix-fallback-button-container">
-          <a className="sellix-fallback-button" href="https://embed.sellix.io/product/64851e9501bbf?" target="_blank" rel="noopener noreferrer">Go to product</a>
-        </div>
-        <div className="sellix-iframe-wrapper">
-          <div className="sellix-iframe-content">
-            <iframe
-              scrolling="auto"
-              src="https://embed.sellix.io/product/64851e9501bbf?"
-              className="sellix-iframe"
-              id="sellix-iframe"
-              onError={(e) => console.log(e)}
-            ></iframe>
+      return (
+        <div id="sellix-container">
+          <div id="sellix-buttons-pointers-fix">
+            <style>
+              {`
+                /* Paste the CSS code here */
+              `}
+            </style>
+          </div>
+          <div id="sellix-modal-64851e9501bbf" style={{ display: 'flex', position: 'fixed', top: '0px', left: '0px', width: '100%', height: '100%', zIndex: '99999' }}>
+            <div id="backdrop" className="sellix-backdrop"></div>
+            <style>
+              {`
+                /* Paste the CSS code here */
+              `}
+            </style>
+            <div className="sellix-iframe-loader-container">
+              <img src="https://cdn.sellix.io/static/embed/loader.png" alt="Loader" className="sellix-iframe-loader" style={{ width: '35px', display: 'none' }} />
+            </div>
+            <div className="sellix-fallback-button-container">
+              <a className="sellix-fallback-button" href="https://embed.sellix.io/product/64851e9501bbf?" target="_blank" rel="noopener noreferrer">Go to product</a>
+            </div>
+            <div className="sellix-iframe-wrapper">
+              <div className="sellix-iframe-content">
+                <iframe
+                  scrolling="auto"
+                  src="https://embed.sellix.io/product/64851e9501bbf?"
+                  className="sellix-iframe"
+                  id="sellix-iframe"
+                  onError={(e) => console.log(e)}
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      );
+    };
+    
+
+    
       
     const { t } = useTranslation();
     return <>
+    {
+        modalOpen === true ? (
+            <SellixEmbed />
+        ) : null
+        
+    }
+
     <script src="https://cdn.sellix.io/static/js/embed.js"></script>
      <div className="mt-5 flex justify-center items-center" id="cheats">
     <span className="text-3xl font-semibold dark:text-white text-black">{t('cards.title')}</span>
